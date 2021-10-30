e33b011cc6f421df2ee110f63fbeb1d25ee41bcb
= <=>
2021-10-30 11:44:41 +0300

    creating the timeline posts

---
+++ InstagramClone/InstagramClone/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Rayan Taj on 30/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSource  {
+    
+    var instagramAccount = [
+    Account(accountName: "rayantaj", profileImage: "avatar", imagesArr: ["jed1" , "jed2" , "jed3"])
+
+    ]
+    
+    
+    @IBOutlet weak var tableViewController: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return instagramAccount.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let customPostCell = tableView.dequeueReusableCell(withIdentifier: "postCustomCell") as! postTableViewCell
+        
+        customPostCell.accountName.text = instagramAccount[indexPath.row].accountName
+        
+        customPostCell.avatar.image = UIImage(named: instagramAccount[indexPath.row].profileImage)
+        
+//        customPostCell.avatar.layer.masksToBounds = true
+//        customPostCell.avatar.layer.cornerRadius = customPostCell.bounds.width / 1
+        
+        customPostCell.imagePost.image = UIImage(named: instagramAccount[indexPath.row].imagesArr[0])
+        
+        return customPostCell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 650
+    }
+
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        tableViewController.delegate = self
+        tableViewController.dataSource = self
+        
+    }
+
+
+}
+
---
+++ InstagramClone/instagramAccount.swift
@@ -0,0 +1,25 @@
+//
+//  instagramAccount.swift
+//  InstagramClone
+//
+//  Created by Rayan Taj on 30/10/2021.
+//
+
+import Foundation
+
+class Account {
+    
+    var accountName : String
+    var profileImage : String
+    var imagesArr : [String]
+    
+    
+    init(accountName : String ,profileImage : String,imagesArr : [String]) {
+        
+        self.accountName = accountName
+        self.profileImage = profileImage
+        self.imagesArr = imagesArr
+        
+    }
+    
+}
